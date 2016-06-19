e964859ca4353f317877483bd67ef95dcc2b246d
Aisha Hasan <ah3252@barnard.edu>
2016-06-19 15:28:09 -0400

    Update CreditCardTester.java

--- CreditCardTester.java
+++ CreditCardTester.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
 
-/**  
-* A tester for the CreditCard class.  
-*  
-* @author Cannon  
-*/ 
-
 public class CreditCardTester {     
     public static void main(String[] args) {         
-	System.out.println("Please enter an 12-digit number.");
+	System.out.println("Enter 12-digit number.");
     	Scanner scanner = new Scanner(System.in); 
     	String creditCardNumber = scanner.next(); 
     	if (creditCardNumber.length() != 12) {     
-		System.out.println("That was not an 12-digit number!");     
+		System.out.println("Not a 12-digit number!");     
 		return; 
      } 
     	CreditCard card = new CreditCard(creditCardNumber); 
     	card.check(); 
     	if (card.isValid()) {     
-		System.out.println("Valid"); 
+		System.out.println("VALID"); 
     } 
     	else {     
 		int code = card.getErrorCode();     
-		System.out.println("Oops! That's an invalid number.");     
-		System.out.println("The error code was: " + code); }
+		System.out.println("Invalid number.");     
+		System.out.println("Error code: " + code); }
     } 
 }
